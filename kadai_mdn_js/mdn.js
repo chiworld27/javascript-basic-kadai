669da38781c73691bdfd8e3aa9f33b47f60efdce
chiworld27 <chiworld27@gmail.com>
2025-08-29 15:07:30 +0900

    Delete kadai_mdn_js directory

--- kadai_mdn_js/mdn.js
+++
@@ -1,8 +0,0 @@
-const date = new Date("2000-01-17T16:45:30");
-const [month, day, year] = [
-  date.getMonth() + 1,
-  date.getDate(),
-  date.getFullYear(),
-];
-
-console.log('${year}年${month}月${day}日');
